37c50f50565f175602d7059821d9c7cfcecb8c65
Arnab Mondal <arnab2807@gmail.com>
2019-08-04 16:13:51 +0530

    Initial setup

--- src/main/java/com/lms/svc/eureka/LmsEurekaServerApplication.java
+++ src/main/java/com/lms/svc/eureka/LmsEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.lms.svc.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class LmsEurekaServerApplication {
 
 	public static void main(String[] args) {
