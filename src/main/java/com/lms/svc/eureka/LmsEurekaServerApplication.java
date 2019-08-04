293c348c9cac2d376b9596ec8b81eb867d4313bc
Arnab Mondal <arnab2807@gmail.com>
2019-08-04 15:00:42 +0530

    Initial commit

---
+++ src/main/java/com/lms/svc/eureka/LmsEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.lms.svc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsEurekaServerApplication.class, args);
+	}
+
+}
